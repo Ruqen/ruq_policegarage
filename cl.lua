589801db89d77eb451f2c5f4c35cb7de4f259e3a
Ruqen <73693641+Ruqen@users.noreply.github.com>
2021-01-27 15:29:06 +0300

    Update cl.lua

--- cl.lua
+++ cl.lua
@@ -4,7 +4,7 @@ local sleep = 2500
 
 Citizen.CreateThread(function()
 	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
 		Citizen.Wait(0)
     end
 end)
